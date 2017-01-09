12de6f6ec9792861bad231999d883ae3aabf57bc
Gus Beirne <gusbeirne@Guss-MacBook.local>
2017-01-09 14:01:30 -0500

    creates entity.js and entity_templates.js

--- assets/util.js
+++ assets/util.js
@@ -1,5 +1,14 @@
 Game.util = {
 
+  randomString: function (len) {
+    var charSource = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.split('');
+    var res='';
+    for (var i=0; i<len; i++) {
+        res += charSource.random();
+    }
+    return res;
+  },
+
   init2DArray: function (x,y,initVal) {
     var a = [];
     for (var xdim=0; xdim < x; xdim++) {
